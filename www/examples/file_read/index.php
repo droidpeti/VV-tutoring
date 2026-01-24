fa7c6c07e3de1a291788e63d7707a54f52bb7f37
Ballai Péter <ballaipeterlorant@gmail.com>
2026-01-24 15:21:35 +0100

    Updated document title of the fruit file reading PHP project

--- www/examples/file_read/index.php
+++ www/examples/file_read/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Fruit File Read</title>
 </head>
 <body>
     <h1>Contents of the fruits file</h1>
