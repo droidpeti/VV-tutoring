1cd719653b367359352b0ae2f0e272cac3a8fc47
Ballai Péter <ballaipeterlorant@gmail.com>
2026-01-24 16:18:31 +0100

    Added C# project for reading a file and storing it in an array

---
+++ C#/examples/file/FileReadStore/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace FileReadStore;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        string file_name = "fruits.txt";
+        // Állíts be egy nagy méretet a tömbödnek, ahol biztos elfér az összes adat
+        string[] fruits = new string[100];
+        // Csinálj egy számlálót, ahol azt tároljuk hogy hány eleme lesz a tömbünknek
+        int db = 0;
+        FileStream fs = new FileStream(file_name, FileMode.Open);
+        StreamReader sr = new StreamReader(fs);
+        while (!sr.EndOfStream)
+        {
+            // A létrehozott számlálót használd a sorok eltárolásához, és ezt nőveld ahogy olvasod a fájlt
+            string line = sr.ReadLine();
+            fruits[db++] = line;
+        }
+        fs.Close();
+        sr.Close();
+
+        Console.WriteLine("A beolvasott gyümölcsök:");
+        for(int i = 0; i < db; i++)
+        {
+            Console.WriteLine(fruits[i]);
+        }
+    }
+}
